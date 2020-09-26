e814a10851f339581fbb806d5e12e82198ab8c52
B3zkurt <68972804+B3zkurt@users.noreply.github.com>
2020-09-27 00:16:06 +0300

    Add files via upload

---
+++ efkar.py
@@ -0,0 +1,58 @@
+import webbrowser
+
+print("-----------------------------------")
+print("      Bozkurt Efkar Toolu :(       ")
+print("                                   ")
+print("-----------------------------------")
+print()
+
+print("1.Müslüm baba Unutamadım")
+print("2.Ruhumda Sızı")
+print("3.Aldırma Gönül")
+print("4.Öldümde Uyandım Gülüm")
+print("5.Kendine iyi bak")
+print("6.Adaletin Bumu Dünya")
+print("7.Ben Yoruldum Hayat")
+print("8.Müslüm Baba Affet")
+print("9.Müslüm Baba Ayrılık Acı Bir şey")
+print("10.Müslüm Baba Hangimiz Sevmedik")
+print("11.Ali Kınık Üzülmedin mi?")
+
+
+efkar = input("ne istersin be gardaşım :")
+
+if efkar =="1":
+    webbrowser.open_new("https://www.youtube.com/watch?v=lYNMQBKDhwg")
+
+if efkar =="2":
+    webbrowser.open_new("https://www.youtube.com/watch?v=67UWF7eMSi8")
+
+if efkar =="3":
+    webbrowser.open_new("https://www.youtube.com/watch?v=8yk0dNCsN2Y")
+
+if efkar =="4":
+    webbrowser.open_new("https://www.youtube.com/watch?v=wgNQ65MippE")
+
+if efkar =="5":
+    webbrowser.open_new("https://www.youtube.com/watch?v=Oxx2BsC7jEc&t=58s")
+
+if efkar =="6":
+    webbrowser.open_new("https://www.youtube.com/watch?v=wt2wOLbRKDA")
+
+if efkar =="7":
+    webbrowser.open_new("https://www.youtube.com/watch?v=K7z3590-Mt0")
+
+if efkar =="8":
+    webbrowser.open_new("https://www.youtube.com/watch?v=2YYLr6yVruQ")
+
+if efkar =="9":
+    webbrowser.open_new("https://www.youtube.com/watch?v=cITUab_6sVs")
+
+if efkar =="10":
+    webbrowser.open_new("https://www.youtube.com/watch?v=IRIMVXyAvw0")
+
+if efkar =="11":
+    webbrowser.open_new("https://www.youtube.com/watch?v=HctKOy1DXCc")
+    
+
+      
